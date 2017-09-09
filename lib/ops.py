29438ccb0a4b0483e6c44b463e9e07ab9490f741
HTY886 <sivia89024@gmail.com>
2017-09-09 22:30:34 +0800

    first commit

---
+++ lib/ops.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+def batch_to_time_major(inputs,split_size):
+    inputs = tf.split(inputs,  num_or_size_splits=split_size ,axis=1)
+    inputs = [tf.squeeze(e,axis=1) for e in inputs]
+    return inputs
+    
+def weight_variable(shape,std_dev):
+    initial = tf.truncated_normal(shape, stddev=std_dev)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.0, shape=shape)
+    return tf.Variable(initial)
